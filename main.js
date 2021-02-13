a65c8a53602dc0fc818b9777c3289537f9b00d9b
MohamedTalaat97 <mohamedtalaat0111790@@gmail.com>
2021-02-13 14:49:18 +0200

    done

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // movie class 
-
-class movie {
+class Movie {
 
     constructor(title, year) {
         this.title = title;
@@ -11,10 +10,10 @@ class movie {
 // ui class 
 
 class UI {
-
+    // 
     static displayMovies() {
-        const storedMovies = [{ title: "Enola Holmes", year: 2020 }, { title: "The Irishman", year: 2019 }];
-        const movies = storedMovies;
+
+        const movies = Store.getMovies();
         movies.forEach(movie => UI.addMovie(movie))
 
     }
@@ -29,6 +28,24 @@ class UI {
         list.appendChild(row);
     }
 
+    static deleteMovie(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#movie-form');
+        container.insertBefore(div, form);
+
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
     static clear() {
 
         document.querySelector('#title').value = '';
@@ -38,17 +55,98 @@ class UI {
 }
 
 
-// store class
 
+// Store Class: Handles Storage
+class Store {
+    static getMovies() {
+        let movies;
+        if (localStorage.getItem('movies') === null) {
+            movies = [];
+        } else {
+            movies = JSON.parse(localStorage.getItem('movies'));
+        }
+
+        return movies;
+    }
+
+    static addMovie(movie) {
+        const movies = Store.getMovies();
+        movies.push(movie);
+        localStorage.setItem('movies', JSON.stringify(movies));
+    }
+
+    static removeMovie(title) {
+        const movies = Store.getMovies();
+
+        movies.forEach((movie, index) => {
+            if (book.title === title) {
+                movies.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('movies', JSON.stringify(movies));
+    }
+}
 // events   display - add - remove
 
-document.addEventListener("DOMContentLoaded", UI.displayMovies)
+document.addEventListener('DOMContentLoaded', UI.displayMovies);
 
+// Event: Add a Book
+document.querySelector('#movie-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const year = document.querySelector('#year').value;
+
+
+    // Validate
+    if (title === '' || year === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        // Instatiate book
+        const movie = new Movie(title, year);
+
+        // Add Book to UI
+        UI.addMovie(movie);
+
+        // Add book to store
+        Store.addMovie(movie);
+
+        // Show success message
+        UI.showAlert('Movie Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+});
+
+// Event: Remove a Book
+document.querySelector('#movie-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteMovie(e.target);
+
+    // Remove book from store
+    Store.removeMovie(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+
+    // Show success message
+    UI.showAlert('Movie Removed', 'success');
+});
+
+
+
+
+
+
+
+
+/*
 
 // get movie from api on search
-document.querySelector('#movie-form').addEventListener('submit', (e) => {
+document.querySelector('#form').addEventListener('submit', (e) => {
     e.preventDefault()
-        // get form values
+    // get form values
     const title = document.querySelector('#title').value;
     const year = document.querySelector('#year').value;
 
@@ -66,3 +164,4 @@ document.querySelector('#movie-form').addEventListener('submit', (e) => {
         .then(data => console.log(data));
 
 })
+*/
